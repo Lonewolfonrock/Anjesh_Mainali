f5bd63385cf1f325900acf682c62877454854946
Lonewolfonrock <110655331+Lonewolfonrock@users.noreply.github.com>
2023-12-01 15:00:44 +0545

    update
    update

--- src/Data/projectdata.tsx
+++ src/Data/projectdata.tsx
@@ -5,7 +5,6 @@ import bussinesscard from "../assets/Images/Screenshot (99).png"
 import todolist from "../assets/Images/Screenshot (101).png"
 import unit from "../assets/Images/Screenshot (102).png"
 import password from "../assets/Images/Screenshot (103).png"
-import Nepal from "../assets/Images/Screenshot (105).png"
 
 
 
